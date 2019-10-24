5d426c51fce9e7fde1cfa78f5274a5520466fc8f
JV Pabalan <jvv.pababalan@gmail.com>
2019-10-24 20:11:07 +0800

    initial

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription, Observable } from 'rxjs';
+
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+import { ThrowStmt } from '@angular/compiler';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit, OnDestroy {
+  recipes: Recipe[];
+  recipes$: Observable<Recipe[]>;
+  subscription: Subscription;
+
+  constructor(private recipeService: RecipeService,
+              private router: Router,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.subscription = this.recipeService.recipesChanged
+      .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipes = recipes;
+        }
+      );
+    this.recipeService.getRecipes().subscribe();
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  firebaseAPIKey: 'AIzaSyCw0oHQzNcbaFEbIevQYOt7TB9Ef7YTZ7M'
+};
